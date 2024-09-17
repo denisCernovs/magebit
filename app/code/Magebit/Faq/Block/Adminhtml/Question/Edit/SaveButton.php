42d6f893eae391b9f14e691e8d944d94f171e5a2
Denis <denis.cernovs@magebit.com>
2024-09-17 09:09:39 +0300

    Added Save and Close button

--- app/code/Magebit/Faq/Block/Adminhtml/Question/Edit/SaveButton.php
+++ app/code/Magebit/Faq/Block/Adminhtml/Question/Edit/SaveButton.php
@@ -7,6 +7,7 @@
 namespace Magebit\Faq\Block\Adminhtml\Question\Edit;
 
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Magento\Ui\Component\Control\Container;
 
 class SaveButton extends GenericButton implements ButtonProviderInterface
 {
@@ -32,6 +33,44 @@ class SaveButton extends GenericButton implements ButtonProviderInterface
                     ]
                 ]
             ],
+            'class_name' => Container::SPLIT_BUTTON,
+            'options' => $this->getOptions(),
+            'dropdown_button_aria_label' => __('Save options'),
         ];
+    }
+
+    /**
+     * Retrieve options
+     *
+     * @return array
+     */
+    private function getOptions()
+    {
+        $options = [
+            [
+                'id_hard' => 'save_and_close',
+                'label' => __('Save & Close'),
+                'data_attribute' => [
+                    'mage-init' => [
+                        'buttonAdapter' => [
+                            'actions' => [
+                                [
+                                    'targetName' => 'magebit_faq_form.magebit_faq_form',
+                                    'actionName' => 'save',
+                                    'params' => [
+                                        true,
+                                        [
+                                            'back' => 'close'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return $options;
     }
 }
